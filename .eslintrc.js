2d86e5e8e29ef99c1df21105022c24316f93de07
leila237 <mbiydzenyuyeileen@gmail.com>
2025-08-13 13:47:55 +0100

    added husky setup

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  extends: [
+    '@react-native-community',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': ['error'],
+    '@typescript-eslint/no-unused-vars': ['warn'],
+  },
+}
