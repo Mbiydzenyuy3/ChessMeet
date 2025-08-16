d3bff7bf0cbb4e13a7f9847b91f8b7589c4378a6
akuma19 <kellyhandy31@gmail.com>
2025-08-16 13:31:20 +0100

    feat(pull): pulled the dev branch

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,6 +7,7 @@ import { store } from '../redux/store'; // adjust path to where your store is
 
 import OTPVerify from './auth/OTPverify';
 import SignIn from './auth/SignIn';
+import PlayLocal from './game/PlayLocal';
 import GetStarted from './index';
 // import Lobby from './lobby/lobby';
 
@@ -19,6 +20,7 @@ export default function RootLayout() {
         <Stack.Screen name="GetStarted" component={GetStarted} />
         <Stack.Screen name="SignIn" component={SignIn} />
         <Stack.Screen name="OTPVerify" component={OTPVerify} />
+        <Stack.Screen name="Local" component={PlayLocal} />
         {/* <Stack.Screen name="Lobby" component={Lobby} /> */}
       </Stack.Navigator>
     </Provider>
--- components/Board.tsx
+++ components/Board.tsx
@@ -1,56 +1,57 @@
+import { Square } from 'chess.js';
 import { useCallback, useRef, useState } from 'react';
 import { Pressable, StyleSheet, View } from 'react-native';
 import Chessboard, { ChessboardRef } from 'react-native-chessboard';
 
 export default function Board() {
   const chessboardRef = useRef<ChessboardRef>(null);
-  const [selectedSquare, setSelectedSquare] = useState<string | null>(null);
+  const [selectedSquare, setSelectedSquare] = useState<Square | null>(null);
 
-  //click logic
-  const handlePress = useCallback(async (square: string) => {
-    //first click select initial box
-    if (!selectedSquare) {
-      setSelectedSquare(square);
-    } else {
-      //second click to move
-      const moveRes = await chessboardRef.current?.move({
-        from: selectedSquare,
-        to: square,
-      });
+  const handlePress = useCallback(
+    async (box: Square) => {
+      if (!selectedSquare) {
+        setSelectedSquare(box);
+      } else {
+        const moveRes = await chessboardRef.current?.move({
+          from: selectedSquare,
+          to: box,
+        });
 
-      //reset move if it's illegal
-      if (!moveRes) {
+        if (!moveRes) {
+          setSelectedSquare(null);
+          return;
+        }
+
         setSelectedSquare(null);
-        return;
       }
-
-      //reset selected box
-      setSelectedSquare(null);
-    }
-  }, []);
-
-  //make all boxes listen for taps and handlepress function
+    },
+    [selectedSquare]
+  );
 
   const listenTap = () => {
     const square = [];
     const box = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
 
-    for (let i = 8; i >= 8; i--) {
+    for (let i = 8; i >= 1; i--) {
       for (let x = 0; x < 8; x++) {
-        const squares = `${box[x]}${i}`;
-        square.push(<Pressable key={squares} onPress={() => handlePress(squares)} />);
+        const squares = `${box[x]}${i}` as Square;
+        square.push(
+          <Pressable
+            key={squares}
+            onPress={() => handlePress(squares)}
+            style={styles.overlaySquare}
+          />
+        );
       }
     }
 
-    return <View>{square}</View>;
+    return square;
   };
 
   return (
     <View style={styles.container}>
       <Chessboard ref={chessboardRef} durations={{ move: 1000 }} />
-      <View style={styles.overlay} pointerEvents="auto">
-        {listenTap()}
-      </View>
+      <View style={styles.overlay}>{listenTap()}</View>
     </View>
   );
 }
@@ -60,7 +61,6 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    position: 'relative',
   },
   overlay: {
     position: 'absolute',
@@ -68,6 +68,12 @@ const styles = StyleSheet.create({
     left: 0,
     right: 0,
     bottom: 0,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
     zIndex: 1,
+  },
+  overlaySquare: {
+    width: `${100 / 8}%`,
+    height: `${100 / 8}%`,
   },
 });
